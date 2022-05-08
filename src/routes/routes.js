29f57b1a8ce903c5083686ae9c0a893195e77a5a
aguskertha <baguskertha@gmail.com>
2022-05-08 13:05:45 +0700

    route root setting

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const topicRouter = require('./topic.route');
 const projectRouter = require('./project.route');
+const indexRouter = require('./index.route');
 
+router.use('/', indexRouter);
 router.use('/topic', topicRouter);
 router.use('/project', projectRouter);
 
