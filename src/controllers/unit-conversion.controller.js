c8db72013c1634f80fa1972adfa20903f0ba273f
aguskertha <baguskertha@gmail.com>
2022-08-27 11:11:50 +0700

    feat(unit): update, ini, delete unit conversion

---
+++ src/controllers/unit-conversion.controller.js
@@ -0,0 +1,88 @@
+const ObjectID = require('mongodb').ObjectId;
+const UnitConversion = require('./../models/unit_conversion.model');
+
+const renderAdminUnitConversion = async (req, res, next) =>{
+    try {
+        const unitConversions = await UnitConversion.find().sort({created: -1})
+        if(unitConversions.length <= 0) throw 'Unit Conversion not found!'
+        res.render('UnitConversion/unit-conversion-list', {
+            layout: 'layouts/main-layout',
+            unitConversions
+        });
+    } catch (error) {
+        res.render('error', {
+            layout: 'layouts/main-layout',
+            message: error,
+            status: 400
+        });
+    }
+}
+
+const updateAdminUnitConversion = async (req, res, next) => {
+    try {
+        console.log(req.body)
+        const {param, unitConversionID, value} = req.body
+        const unitConversion = await UnitConversion.findOne({_id: ObjectID(unitConversionID)})
+        if(!unitConversion) throw 'Unit Conversion not found!'
+        switch (param) {
+            case "KGtoLitre":
+                await UnitConversion.updateOne({ _id: unitConversionID},{$set: {KGtoLitre: value}});
+                break;
+            case "CBMtoLitre":
+                await UnitConversion.updateOne({ _id: unitConversionID},{$set: {CBMtoLitre: value}});
+                break;
+            case "KGtoMMBTU":
+                await UnitConversion.updateOne({ _id: unitConversionID},{$set: {KGtoMMBTU: value}});
+                break;
+            case "CBMtoMT":
+                await UnitConversion.updateOne({ _id: unitConversionID},{$set: {CBMtoMT: value}});
+                break;
+            case "USDtoIDR":
+                await UnitConversion.updateOne({ _id: unitConversionID},{$set: {USDtoIDR: value}});
+                break;
+        
+            default:
+                break;
+        }
+        res.redirect('/unit-conversion/admin')
+
+    } catch (error) {
+        res.render('error', {
+            layout: 'layouts/main-layout',
+            message: error,
+            status: 400
+        });
+    }
+}
+
+const initUnitConversion = async (req, res, next) => {
+    try {
+        const unitConversion = {
+            KGtoLitre: 1.777,
+            CBMtoLitre: 0.001,
+            KGtoMMBTU: 0.046452,
+            CBMtoMT: 0.56274620146314,
+            USDtoIDR: 14500,
+        }
+        const newUnitConversion = new UnitConversion(unitConversion)
+        await newUnitConversion.save()
+        res.json({message: "Unit Conversion Created"})
+    } catch (error) {
+        res.status(400).json({message: error.toString()})
+    }
+}
+const deleteUnitConversions = async (req, res, next) => {
+    try {
+        await UnitConversion.deleteMany()
+        res.json({message: "Unit Conversion Deleted!"})
+    } catch (error) {
+        res.status(400).json({message: error.toString()})
+    }
+}
+
+module.exports = {
+    renderAdminUnitConversion,
+    updateAdminUnitConversion,
+    initUnitConversion,
+    deleteUnitConversions
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ const indexRouter = require('./index.route');
 const userRouter = require('./user.route');
 const typeVoyageRouter = require('./type-voyage.route');
 const typeFreightRouter = require('./type-freight.route');
+const unitConversionRouter = require('./unit-conversion.route');
 const {ensureAuthenticated} = require('./../middleware/auth');
 
 router.use('/', indexRouter);
@@ -13,5 +14,6 @@ router.use('/project', ensureAuthenticated, projectRouter);
 router.use('/user', userRouter);
 router.use('/type-voyage', ensureAuthenticated, typeVoyageRouter);
 router.use('/type-freight', ensureAuthenticated, typeFreightRouter);
+router.use('/unit-conversion', ensureAuthenticated, unitConversionRouter);
 
 module.exports = router;
